5d32dfb5162cd04f4349ab208bafb9f3b119e98b
ZacksGithubAccount <113447004+ZacksGithubAccount@users.noreply.github.com>
2023-11-05 16:36:21 -0500

    Half-finished Scripts
    This adds mostly nonfunctional scripts for spawning objects in random spots, collision code, and movement.

---
+++ OutOfTimeJam/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+{
+    public ContactFilter2D filter;
+    public Collider2D playerCollider;
+    public Collider2D badCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        filter.NoFilter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (badCollider.IsTouching(playerCollider, filter))
+        {
+            die();
+        }
+    }
+    void die()
+    {
+        //game over or restart
+    }
+}
